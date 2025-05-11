db945ae1dce1c6eee05cf9fa2298c544bfc45b31
Onur Degerli <onur.degerli@viviotex.com>
2025-05-11 21:49:27 +0200

    Update the aws stack

--- lib/aws-cdk-app-stack.ts
+++ lib/aws-cdk-app-stack.ts
@@ -1,16 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
+    
     // The code that defines your stack goes here
-
+    
     // example resource
     // const queue = new sqs.Queue(this, 'AwsCdkAppQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    
+    new Bucket(this, 'MyFirstBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
   }
 }
